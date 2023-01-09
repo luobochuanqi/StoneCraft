e27f083cf7b32f48240b3833147937bfb9604bab
luobochuanqi <luobochuanqi@qq.com>
2023-01-09 17:16:19 +0800

    测试

---
+++ java/xyz/luobochuanqi/StoneCraft/ModUtils/Utils.java
@@ -0,0 +1,6 @@
+package xyz.luobochuanqi.StoneCraft.ModUtils;
+
+public class Utils {
+    public static final String ModId = "stonecraft";
+    public static final String ModV = "v1.0.1";
+}
--- java/xyz/luobochuanqi/StoneCraft/StoneCraft.java
+++ java/xyz/luobochuanqi/StoneCraft/StoneCraft.java
@@ -5,20 +5,11 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import xyz.luobochuanqi.StoneCraft.Register.*;
-import xyz.luobochuanqi.StoneCraft.Screen.CrusherScreen;
 
 @Mod("stonecraft")
 public class StoneCraft {
     public StoneCraft() {
         ItemRegister.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        BlockRegister.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        BlockEntityRegister.BLOCK_ENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
-        MenuRegister.MENUS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        RecipeRegister.register(FMLJavaModLoadingContext.get().getModEventBus());
-    }
-    private void clientSetup(final FMLClientSetupEvent event) {
-        //添加这个，第一个参数机器方块的GUI，后面是这个机器方块的screen
-        MenuScreens.register(MenuRegister.CrusherMenu.get(), CrusherScreen::new);
     }
 }
 
